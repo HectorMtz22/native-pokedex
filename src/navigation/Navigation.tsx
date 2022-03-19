ce6e2412bfef44ad9918139e2db8e35bbdfe462e
HectorMtz22 <hec.gre81@gmail.com>
2022-03-18 22:29:42 -0600

    Styling and configuring icons for bottom tab

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,4 +1,6 @@
+import { Image } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import Account from "../screens/Account";
 import Pokedex from "../screens/Pokedex";
 import Favorite from "../screens/Favorite";
@@ -8,9 +10,36 @@ const { Navigator, Screen } = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <Navigator>
-      <Screen name="Account" component={Account} />
-      <Screen name="Pokedex" component={Pokedex} />
-      <Screen name="Favorite" component={Favorite} />
+      <Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: (props) => <Icon name="heart" {...props} />,
+        }}
+      />
+      <Screen
+        name="Pokedex"
+        component={Pokedex}
+        options={{ tabBarLabel: "", tabBarIcon: () => renderPokeball() }}
+      />
+      <Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Mi cuenta",
+          tabBarIcon: (props) => <Icon name="user" {...props} />,
+        }}
+      />
     </Navigator>
+  );
+}
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require("../assets/pokeball.png")}
+      style={{ width: 75, height: 75, top: -15 }}
+    />
   );
 }
