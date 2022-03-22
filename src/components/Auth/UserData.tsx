3563e5d7d489b9dd341060de53e4aef1972b45d9
HectorMtz22 <hec.gre81@gmail.com>
2022-03-21 21:33:00 -0600

    Add sth on userdata

--- src/components/Auth/UserData.tsx
+++ src/components/Auth/UserData.tsx
@@ -1,6 +1,9 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function UserData() {
+  const { auth, logout } = useContext(AuthContext);
   return (
     <View>
       <Text>UserData</Text>
