42fc21eb8562fb62b52ffc37908d674cff388f0d
HectorMtz22 <hec.gre81@gmail.com>
2022-03-21 18:52:50 -0600

    Add LoginForm integration with Formik and Yup

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -6,26 +6,54 @@ import {
   Button,
   Keyboard,
 } from "react-native";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues,
+    validationSchema: Yup.object(validationSchema),
+    validateOnChange: false,
+    onSubmit: (formValue) => console.log("Enviado"),
+  });
+
   return (
     <View>
       <Text style={styles.title}>Iniciar Sesión</Text>
+      <Text style={styles.error}>{formik.errors.username}</Text>
       <TextInput
         placeholder="Nombre de usuario"
         style={styles.input}
         autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
+        autoFocus
       />
+      <Text style={styles.error}>{formik.errors.password}</Text>
       <TextInput
         placeholder="Contraseña"
         autoCapitalize="none"
         style={styles.input}
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
       />
-      <Button title="Entrar" onPress={() => console.log("Hi")} />
+      <View style={styles.button}>
+        <Button title="Entrar" onPress={() => formik.handleSubmit()} />
+      </View>
     </View>
   );
 }
+
+const initialValues = {
+  username: "",
+  password: "",
+};
+
+const validationSchema = {
+  username: Yup.string().required("El usuario es obligatorio"),
+  password: Yup.string().required("La contraseña es obligatoria"),
+};
 
 const styles = StyleSheet.create({
   title: {
@@ -35,11 +63,20 @@ const styles = StyleSheet.create({
     marginTop: 50,
     marginBottom: 15,
   },
+  error: {
+    // textAlign: "center",
+    paddingHorizontal: 20,
+    fontWeight: "bold",
+    color: "#f00",
+  },
   input: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
+  },
+  button: {
+    marginHorizontal: 12,
   },
 });
