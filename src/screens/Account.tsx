bbef7b33f31b57e7ada41481f64fb64e0167885a
HectorMtz22 <hec.gre81@gmail.com>
2022-03-21 21:28:13 -0600

    Replace const auth null for the useContext auth

--- src/screens/Account.tsx
+++ src/screens/Account.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native";
+import { useContext } from "react";
+import { View } from "react-native";
 import UserData from "../components/Auth/UserData";
 import LoginForm from "../components/Auth/LoginForm";
+import { AuthContext } from "../context/AuthContext";
 
 export default function Account() {
-  const auth = null;
+  const { auth } = useContext(AuthContext);
 
   return <View>{auth ? <UserData /> : <LoginForm />}</View>;
 }
